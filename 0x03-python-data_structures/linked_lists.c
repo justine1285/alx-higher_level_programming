9e05fa52e24ae82e78c3c44f00c80335307ce56d
justine1285 <justineadugbo@gmail.com>
2023-12-02 17:48:58 -0800

    update

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -11,7 +11,7 @@ size_t print_listint(const listint_t *h)
 {
 	const listint_t *current;
 	unsigned int n; /* number of nodes */
-	
+
 	current = h;
 	n = 0;
 	while (current != NULL)
@@ -33,16 +33,16 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *current;
-	
+
 	current = *head;
-	
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	
+
 	new->n = n;
 	new->next = NULL;
-	
+
 	if (*head == NULL)
 		*head = new;
 	else
@@ -51,7 +51,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			current = current->next;
 		current->next = new;
 	}
-	
+
 	return (new);
 }
 
@@ -63,7 +63,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 void free_listint(listint_t *head)
 {
 	listint_t *current;
-	
+
 	while (head != NULL)
 	{
 		current = head;

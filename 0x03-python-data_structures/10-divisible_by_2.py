be499c4940ddf9717e7dbad130a242fed5c983e7
justine1285 <justineadugbo@gmail.com>
2023-12-02 16:55:40 -0800

    only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#1/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = my_list.copy()
+    for k in range(len(my_list)):
+        if my_list[k] % 2 == 0:
+            new_list[k] = True
+        else:
+            new_list[k] = False
+    return new_list

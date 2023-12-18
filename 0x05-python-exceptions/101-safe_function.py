ee2d0b5c5621e12fd1c6df86c3a049ee24c5d747
justine1285 <justineadugbo@gmail.com>
2023-12-18 05:22:48 -0800

    safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    """
+    executes a function safely
+
+    Args:
+        fct: function to be executed
+        args: arbitaray argument lists
+
+    Return:
+        result of function executed, otherwise None
+    """
+    try:
+        return fct(*args)
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+        return None
+

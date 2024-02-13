773d4b928bfd2f0deccf3811d2c002947650850e
Justine1285 <justineadugbo@gmail.com>
2024-02-13 09:35:06 -0800

    full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- This script creates a table second_table in the database hbtn_0c_0
+-- and add multiplication rows
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);

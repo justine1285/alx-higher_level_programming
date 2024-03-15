04318aa31fc30aa03619f293a3cd5a38646e8799
Justine1285 <justineadugbo@gmail.com>
2024-03-15 04:38:16 -0700

    13 model

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for state in session.query(State).filter(State.name.like('%a%'));
+    for state in session.query(State).filter(State.name.like('%a%')):
         session.delete(state)
     session.commit()
     session.close()

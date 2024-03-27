9886d35626530772e9ed950fdb1c5292fd5e4c79
Justine1285 <justineadugbo@gmail.com>
2024-03-27 12:58:46 -0700

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
-"""Find a peak"""
+"""Algorithms for list of integers."""
 
 
-def find_peak(intlist):
-    """Finds a peak function"""
-
-    if intlist is None or intlist == []:
-        return None
-    largo = len(intlist)
-    medio = ((largo) // 2)
-    medio = int(medio)
-    if largo == 1:
-        return intlist[0]
-    if largo == 2:
-        return max(intlist)
-    if intlist[medio] >= intlist[medio - 1] and\
-            intlist[medio] >= intlist[medio + 1]:
-        return intlist[medio]
-    if medio > 0 and intlist[medio] < intlist[medio + 1]:
-        return find_peak(intlist[medio:])
-    if medio > 0 and intlist[medio] < intlist[medio - 1]:
-        return find_peak(intlist[:medio])
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]

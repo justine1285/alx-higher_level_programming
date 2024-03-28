dee2ee05ce958573a7d5c23d28e8291cffe13c46
Justine1285 <justineadugbo@gmail.com>
2024-03-28 06:08:09 -0700

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    r = requests.get('https://api.githun.com/user',
+    r = requests.get('https://api.github.com/user',
                      auth=(sys.argv[1], sys.argv[2]))
     if r.status_code >= 400:
         print('None')

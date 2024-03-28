eabcd2244635847179f13d1dd8e8a2f535b4e379
Justine1285 <justineadugbo@gmail.com>
2024-03-28 06:06:10 -0700

    github

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    r = requests.get('https://api.github.com/repo/{}/{}/commits'
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(sys.argv[2], sys.argv[1]))
     if r.status_code >= 400:
         print('None')
